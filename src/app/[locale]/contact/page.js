6b17b21eab2f21060dde7205d5aa3d4a054bd854
Abdullah Koroglu <abdullah34koroglu@gmail.com>
2024-09-05 13:51:21 +0300

    contact page

--- src/app/[locale]/contact/page.js
+++ src/app/[locale]/contact/page.js
@@ -2,6 +2,7 @@ import Contact from '@/components/ascharge/Contact'
 import locales from '@/locales'
 import Banner from '@/components/products/Banner'
 import React from 'react'
+import DownloadCatalogue from '@/components/ascharge/DownloadCatalogue'
 
 function page({params}) {
   const { locale } = params
@@ -10,6 +11,7 @@ function page({params}) {
     <div>
       <Banner locale={locale} text={currentLocale.contact_form} />
       <Contact locale={locale} />
+      <DownloadCatalogue locale={locale} />
     </div>
   )
 }
