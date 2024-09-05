042e23491cdda1f85e74282aba8b3ee12751b4ab
Abdullah Koroglu <abdullah34koroglu@gmail.com>
2024-09-06 01:10:45 +0300

    went back to hydrating

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -41,7 +41,7 @@ const Navbar = ({ locale }) => {
 
   const menuDetailData = {
     products: {
-      items: handleProductsMenuItems(),
+      items: window ? handleProductsMenuItems() : [],
       showImage: true
     },
     enterprise: {
